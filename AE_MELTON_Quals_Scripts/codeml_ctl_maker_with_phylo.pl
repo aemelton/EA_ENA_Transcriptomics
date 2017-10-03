02ece5d62b2917c3e4aa2a5f36e07f460ad777e6
Anthony E. Melton <aemelton@ufl.edu>
2017-10-03 11:15:56 -0400

    These edits include creating a function to make the nesting within the body of the script easier to follow, as well as comment block explaining the goal of the script.

--- AE_MELTON_Quals_Scripts/codeml_ctl_maker_with_phylo.pl
+++ AE_MELTON_Quals_Scripts/codeml_ctl_maker_with_phylo.pl
@@ -1,5 +1,11 @@
 #!/usr/bin/perl
 
+#This script generates a list of alignment files in the working directory and then creates
+#a codeml ctl file for each one. This script requires access to a template ctl file, for
+#which users may need to edit the path to. This script is targeted toward 
+#analyses using phylogenies (codeml_ctl_maker.pl is simply for pairwise comparisons), and 
+#will aslo require a tree file (a newick format file with the topology).
+
 use warnings;
 
 system 'ls *.phy > AlignmentFiles';
@@ -11,38 +17,44 @@ while (<FH1>)
 		$file = $1;
 		$gene = $2;
 		$OutFile = "$gene.out";
-		$treefile = "$gene.pruned.tre";
+		$treefile = "$gene.tre";
 		
 		open OUT1, ">$gene.ctl";
-		open FH2, "</home/aemelton/Scripts/CODEML_PIPE/codeml_phylo.ctl";
+		open FH2, "<codeml.ctl";
 		while (<FH2>)
 		{
-			$line = $_;
-			chomp $line;
-			if ($line =~ m/.+SEQUENCEFILE.+/ || $line =~ m/.TREEFILE.+/ ||  $line =~ m/.+MLCOUTFILE.+/)
-			{
-				if ($line =~ m/.+(SEQUENCEFILE).+/)
-				{
-					$SEQFILE = $1;
-					$line =~ s/$SEQFILE/$file/;
-					print OUT1 "$line\n";
-				}
-				if ($line =~ m/.+(TREEFILE).+/)
-                                {
-                                        $TREEFILE = $1;
-                                        $line =~ s/$TREEFILE/$treefile/;
-                                        print OUT1 "$line\n";
-                                }
-				if ($line =~ m/.+(MLCOUTFILE).+/)
-				{
-					$OUTFILE = $1;
-					$line =~ s/$OUTFILE/$OutFile/;
-					print OUT1 "$line\n";
-				}
-			}
-			else {print OUT1 "$line\n"}
+		generate_ctl()
 		}
 	}
 }
 exit;
 					
+################################Sub-function below########################################
+sub generate_ctl() {
+	$line = $_;
+	chomp $line;
+	if ($line =~ m/.+SEQUENCEFILE.+/ || $line =~ m/.TREEFILE.+/ ||  $line =~ m/.+MLCOUTFILE.+/)
+	{
+		if ($line =~ m/.+(SEQUENCEFILE).+/)
+		{
+			$SEQFILE = $1;
+			$line =~ s/$SEQFILE/$file/;
+			print OUT1 "$line\n";
+		}
+		if ($line =~ m/.+(TREEFILE).+/)
+        {
+            $TREEFILE = $1;
+            $line =~ s/$TREEFILE/$treefile/;
+            print OUT1 "$line\n";
+        }
+		if ($line =~ m/.+(MLCOUTFILE).+/)
+		{
+			$OUTFILE = $1;
+			$line =~ s/$OUTFILE/$OutFile/;
+			print OUT1 "$line\n";
+		}
+	}
+	else {print OUT1 "$line\n"}
+}
+			
+			
